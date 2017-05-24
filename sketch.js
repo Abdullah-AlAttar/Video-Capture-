6ec22b31a2b04b996c002774a67364dc90adeef4
Abdullah AlAttar <abdullah.alattar@cis.asu.edu.eg>
2017-05-24 12:52:21 +0200

    uploaded wrong code first

--- sketch.js
+++ sketch.js
@@ -1,39 +1,36 @@
-var capture;
-var button;
-var meinHistory =[];
-function setup()
-{
-   createCanvas(1024, 240);
-   background(23);
-   capture=  createCapture(VIDEO);
-   capture.size(320, 240);
-   button =createButton('mienButton');
-   button.mousePressed(onMouseDown);
-  // capture.hide();
+var video;
+var vScale = 14;
+var slider;
+var anotherSlider;
+function setup() {
+  createCanvas(640, 480);
+  //pixelDensity(1);
+  video = createCapture(VIDEO);
+  video.size(width / vScale, height / vScale);
+  slider = createSlider(12, 32, vScale, 0.2);
+  anotherSlider = createSlider(1, 10, 3);
+  //video.hide();
 }
-function onMouseDown()
-{
-  //image(capture, 0,0);
-  meinHistory.push(capture.get());
-}
-function mousePressed()
-{
 
-}
-function draw()
-{
-  var w=80;
-  var h=60;
-  var x=0;
-  var y=0;
-  for (var i = 0; i < meinHistory.length; i++) {
-    tint(255,100);
-    image(meinHistory[i],x,y,w,h);
-    x=x+w;
-    if(x>width)
-    {
-      x=0;
-      y+=h;
+function draw() {
+  vScale = slider.value();
+  video.size(width / vScale, height / vScale);
+  background(23);
+  loadPixels();
+  video.loadPixels();
+  for (var y = 0; y < video.height; ++y) {
+    for (var x = 0; x < video.width; ++x) {
+      var index = (x + y * video.width) * 4;
+      var r = video.pixels[index + 0];
+      var g = video.pixels[index + 1];
+      var b = video.pixels[index + 2];
+      var bright = (r + g + b) / 3;
+      var w = map(bright, 0, 255, 0, vScale);
+      noStroke();  //try commenting this 
+      fill(bright, bright, 100);
+      //try rect or ellipse
+      //rect(x * vScale, y * vScale, w * anotherSlider.value(), w * anotherSlider.value());
+      ellipse(x * vScale, y * vScale, w * anotherSlider.value());
     }
   }
 }
